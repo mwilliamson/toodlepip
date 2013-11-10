54800417a26db7cb71f94eb750b26a66d652d336
Michael Williamson <mike@zwobble.org>
2013-11-10 13:11:33 +0000

    Don't use CLI to test Python builder

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -8,21 +8,7 @@ local = spur.LocalShell()
 
 
 @istest
-def python_project_configured_with_travis_yml_can_be_built():
+def build_command_builds_project_locally():
     result = local.run(["toodlepip", "build", testing.path("minimal-python")])
     assert b"This is CPython 2.7 calling" in result.output, "Output was: {0}".format(result.output)
-    assert_equal(0, result.return_code)
-
-
-@istest
-def python_version_can_be_selected():
-    result = local.run(["toodlepip", "build", testing.path("minimal-python-2.6")])
-    assert b"This is CPython 2.6 calling" in result.output, "Output was: {0}".format(result.output)
-    assert_equal(0, result.return_code)
-
-
-@istest
-def pypy_can_be_used_as_python_version():
-    result = local.run(["toodlepip", "build", testing.path("minimal-python-pypy")])
-    assert b"This is PyPy 2.7 calling" in result.output, "Output was: {0}".format(result.output)
     assert_equal(0, result.return_code)
