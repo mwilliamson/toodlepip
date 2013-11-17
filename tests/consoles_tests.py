44609e0432855506cd977cd39f1a780dfbd9e366
Michael Williamson <mike@zwobble.org>
2013-11-17 14:52:36 +0000

    Add test for stderr output from console

--- tests/consoles_tests.py
+++ tests/consoles_tests.py
@@ -7,9 +7,16 @@ from toodlepip.consoles import Console
 
 
 @istest
-def console_writes_output_to_console():
+def console_writes_stdout_output_to_console():
     console, output = _create_local_console()
     console.run("Action", ["echo", "Go go go!"])
+    assert b"Go go go!" in output.getvalue()
+
+
+@istest
+def console_writes_stderr_output_to_console():
+    console, output = _create_local_console()
+    console.run("Action", ["sh", "-c", "echo 'Go go go!' 1>&2"])
     assert b"Go go go!" in output.getvalue()
 
 
