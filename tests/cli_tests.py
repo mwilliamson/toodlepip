4f12c7c6bd0f02f800a29274293f3c51518cbcba
Michael Williamson <mike@zwobble.org>
2014-02-09 18:23:34 +0000

    Run CLI tests with config in Python

--- tests/cli_tests.py
+++ tests/cli_tests.py
@@ -1,5 +1,9 @@
+import os
+
 from nose.tools import istest, assert_equal
 import spur
+import tempman
+import yaml
 
 from . import testing
 
@@ -9,6 +13,13 @@ local = spur.LocalShell()
 
 @istest
 def build_command_builds_project_locally():
-    result = local.run(["toodlepip", "build", testing.path("travis-minimal")])
+    result = _build_empty(travis_yml={"script": "echo 'Hello, world!'"})
     assert b"Hello, world!" in result.output, "Output was: {0}".format(result.output)
     assert_equal(0, result.return_code)
+
+
+def _build_empty(travis_yml):
+    with tempman.create_temp_dir() as temp_dir:
+        with open(os.path.join(temp_dir.path, ".travis.yml"), "w") as travis_yml_file:
+            yaml.dump(travis_yml, travis_yml_file)
+        return local.run(["toodlepip", "build", temp_dir.path])
